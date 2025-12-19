ce17aaf54609d1dfd9ee07e2494b30e557327140
Ketan <ketand9916@gmail.com>
2025-12-20 01:25:47 +0530

    commit done

--- demo.java
+++ demo.java
@@ -1,11 +1,12 @@
 class Demo {
   public static void main(String[] args) {
     System.out.println("Demo Program Printing Hello World");
-    
 
     String name = "ashokit";
 
     String name ="Credit Card";
+
+    int j = 10;
 
   }
 }
