82194a9283891b8952cf1e88845f44dcc9fa2250
Ketan <ketand9916@gmail.com>
2025-12-20 01:18:31 +0530

    removing unused variable

--- demo.java
+++ demo.java
@@ -1,9 +1,7 @@
 class Demo {
   public static void main(String[] args) {
     System.out.println("Demo Program Printing Hello World");
-    int i = 10;
-
-    int j = 20;
+    
 
     String name = "ashokit";
 
